aeae60af0e94a5fb7096623ebc03dfeb29ab1262
Adrian Jelonek <adeq0990jelonek@gmail.com>
2024-05-22 02:17:48 +0200

    refactor

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,12 +1,11 @@
 import { SelectedPage } from "@/shared/types";
-import { scales } from "chart.js";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-const childVariant ={
-  hidden: {opacity: 0, scale: 0.9},
-  visible: {opacity: 1, scale: 1 }
-}
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
 
 interface Props {
   icon: JSX.Element;
@@ -15,26 +14,31 @@ interface Props {
   setSelectedPage: (value: SelectedPage) => void;
 }
 
-const Benefit = ({icon, title, description, setSelectedPage}: Props) => {
+const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <motion.div variants={childVariant} className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div
+      variants={childVariant}
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+    >
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
         </div>
       </div>
-      
+
       <h4 className="font-bold ">{title}</h4>
       <p className="my-3">{description}</p>
       <AnchorLink
         className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-        onClick={() => {setSelectedPage(SelectedPage.ContactUs);}}
+        onClick={() => {
+          setSelectedPage(SelectedPage.ContactUs);
+        }}
         href={`#{SelectedPage.ContactUs}`}
       >
         <p>Learn More</p>
       </AnchorLink>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Benefit
+export default Benefit;
